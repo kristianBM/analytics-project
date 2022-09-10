b8acb188ed22a7c705aa7f7eea5401631098abd7
Kristian Mello <kristian.bonfim@hotmail.com>
2022-09-09 21:03:19 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -17,8 +17,12 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>([])
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales`).then(response => { setSales(response.data.content) });
-    }, [])
+        const dmin = minDate.toISOString().slice(0, 10)
+        const dmax = maxDate.toISOString().slice(0, 10)
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
+            .then(response => { setSales(response.data.content) });
+    }, [minDate, maxDate])
 
 
     return (
@@ -76,33 +80,6 @@ function SalesCard() {
                                 )
                             })
                         }
-
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">08/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55300.00</td>
-                            <td>
-                                <div className="dsmeta-red-btn-container">
-                                    <NotificationButton />
-                                </div>
-                            </td>
-                        </tr>
                     </tbody>
 
                 </table>
